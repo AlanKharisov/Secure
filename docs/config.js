07663fc89ca3a20a4d5845de91e73dd2274e81d7
AlanKharisov <alankharisov1@gmail.com>
2025-09-03 16:00:38 +0200

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -1,5 +1,4 @@
-<script>
-  window.APP_CONFIG = {
-    API_BASE: "https://app.world-of-photo.com" // твой домен бэка
-  };
-</script>
+// Где крутится бекенд (Render/домен)
+window.APP_CONFIG = {
+  API_BASE: "https://app.world-of-photo.com"
+};
