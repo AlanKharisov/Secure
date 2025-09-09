bfc3e5557eeebd77dc430a4a7668b69a5043c8aa
AlanKharisov <alankharisov1@gmail.com>
2025-09-09 11:28:46 +0200

    Update config.js

--- docs/config.js
+++ docs/config.js
@@ -4,18 +4,11 @@
   const API_BASE = isLocal ? "http://localhost:5000" : "https://app.world-of-photo.com";
   window.API_BASE = API_BASE;
 
-  // Фолбек-адміни (на случай, если /api/me недоступен; обычно не нужно)
-  window.CLIENT_ADMINS = new Set([
-    "alankharisov1@gmail.com",
-  ]);
-
-  // Фолбек для отображения чипсов брендов (если бэкенд временно недоступен)
-  window.EMAIL_BRANDS = {
-    "alankharisov1@gmail.com": [{ slug: "ALAN-KHARISOV", name: "Alan Kharisov", verified: true }],
-    "torosyanemil2310@gmail.com": [{ slug: "EMIL-COLA", name: "Emil Cola", verified: true }],
-  };
-
+  // опціонально: клієнтські фолбеки (зазвичай не потрібні, бекенд дає /api/me)
+  window.CLIENT_ADMINS = new Set([]);
+  window.EMAIL_BRANDS = {};
   window.CLIENT_MANUFACTURERS = new Set([]);
+
   window.APP_CONFIG = Object.freeze({
     API_BASE,
     CLIENT_ADMINS: window.CLIENT_ADMINS,
